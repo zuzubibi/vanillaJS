f48999b9a1347d64264c023712190db1d8236514
zuzubibi <lsb9938@naver.com>
2023-05-06 23:44:53 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 2;
+let myName = 'noco';
+
+console.log(num1 + num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(myName + " hello");
+
+myName = "nicolas";
+console.log("your new name is " + myName);
